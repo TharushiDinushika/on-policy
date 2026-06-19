61386d87542a9df16b5ab66f058a9a95b4ddbeea
TharushiDinushika <ekanayaketd.21@uom.lk>
2026-06-19 21:53:00 +0530

    Fix model loading and evaluation on CPU for rendering

--- onpolicy/runner/separated/base_runner.py
+++ onpolicy/runner/separated/base_runner.py
@@ -198,13 +198,14 @@ class Runner(object):
 
     def restore(self):
         for agent_id in range(self.num_agents):
-            policy_actor_state_dict = torch.load(str(self.model_dir) + '/actor_agent' + str(agent_id) + '.pt')
+            policy_actor_state_dict = torch.load(str(self.model_dir) + '/actor_agent' + str(agent_id) + '.pt', map_location=self.device)
             self.policy[agent_id].actor.load_state_dict(policy_actor_state_dict)
-            policy_critic_state_dict = torch.load(str(self.model_dir) + '/critic_agent' + str(agent_id) + '.pt')
-            self.policy[agent_id].critic.load_state_dict(policy_critic_state_dict)
-            if self.trainer[agent_id]._use_valuenorm:
-                policy_vnrom_state_dict = torch.load(str(self.model_dir) + '/vnrom_agent' + str(agent_id) + '.pt')
-                self.trainer[agent_id].value_normalizer.load_state_dict(policy_vnrom_state_dict)
+            if not self.use_render:
+                policy_critic_state_dict = torch.load(str(self.model_dir) + '/critic_agent' + str(agent_id) + '.pt', map_location=self.device)
+                self.policy[agent_id].critic.load_state_dict(policy_critic_state_dict)
+                if self.trainer[agent_id]._use_valuenorm:
+                    policy_vnrom_state_dict = torch.load(str(self.model_dir) + '/vnrom_agent' + str(agent_id) + '.pt', map_location=self.device)
+                    self.trainer[agent_id].value_normalizer.load_state_dict(policy_vnrom_state_dict)
 
     def log_train(self, train_infos, total_num_steps): 
         if self.use_render:
